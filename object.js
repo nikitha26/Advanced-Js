8caa89a6c040a34a29d69fdc6fa8dd82498c15ee
nikitha26 <mnikitha365@gmail.com>
2021-06-20 19:13:18 +0530

    Advanced js

---
+++ object.js
@@ -0,0 +1,25 @@
+var user = {
+    name:"john",
+    age:20,
+    job:"HR",
+}   
+var user1 = {
+    name:"john",
+    age:20,
+    job:"HR",
+}   
+var user1 = user
+// user.sname = "rock";          //Add new property to the variable
+// for(let key in user)          //for with objects
+console.log(user == user1)
+
+//in method return true or false
+//console.log("compa" in user)
+
+// function makeUser(fname,lname) {
+//     return {
+//         fname:fname,
+//         lname:lname,
+//     }
+// }
+// console.log(makeUser("jo","rock"))
