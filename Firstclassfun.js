ff4943112c4667a233949af06a39eb909cb52bb0
nikitha26 <mnikitha365@gmail.com>
2021-08-06 21:10:17 +0530

    settimeout

---
+++ Firstclassfun.js
@@ -0,0 +1,39 @@
+//a();
+//Function Statement
+// function a(){
+//     console.log("a called");
+// }
+
+//b();
+//Function Expression
+// function hello(){
+    
+//     console.log(a);
+// }
+// hello();
+// console.log("HIII")
+
+
+// setTimeout(function(){
+//    console.log("Call back");
+// },2000)
+
+// function x(y){
+//   console.log("x")
+//   y()
+// }
+// x(function y(){
+//     console.log("y")
+// })
+
+// document.getElementById("btn").addEventListener("click", function xyz(){
+//     console.log("Button Clicked")
+// });
+
+let x = 4
+function squre(){
+    let y = 2;
+    console.log(y);
+}
+squre()
+console.log(y)
---
+++ closuretwo.js
@@ -0,0 +1,27 @@
+// let g = 3;
+
+// function z(){
+//     hello();
+//     let b = 200;
+//     function x(){
+//         let a = 10
+//         function y(){
+//            console.log(a +" This is the closure "+b);
+//         }
+//         y();
+//     }
+//     x();
+// }
+// z();
+
+// function hello(){
+//     console.log(g+"HIIIII")
+// }
+
+
+//Function expression
+var annon = function (){
+    let a = 10
+    console.log(a);
+}
+annon();
